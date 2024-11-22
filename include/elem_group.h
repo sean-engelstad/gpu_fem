bed33da3c1478b48f295b386a39927a15134350c
Sean Engelstad <sean.engelstad1@gmail.com>
2024-11-22 10:55:04 -0500

    attempt verification of plane stress element

--- include/elem_group.h
+++ include/elem_group.h
@@ -1,10 +1,9 @@
 #pragma once
 
+#include "a2dcore.h"
 #include "basis.h"
 #include "geometry.h"
 #include "physics.h"
-
-#include "a2dcore.h"
 
 // include the kernels if on the GPU
 #ifdef USE_GPU
@@ -21,20 +20,17 @@ class ElementGroup {
   using FADType = typename A2D::ADScalar<T, 1>;
 
   static constexpr int32_t xpts_per_elem = Geo::spatial_dim * Geo::num_nodes;
-  static constexpr int32_t dof_per_elem = Phys::vars_per_node * Basis::num_nodes;
+  static constexpr int32_t dof_per_elem =
+      Phys::vars_per_node * Basis::num_nodes;
 
   template <class Data>
   __HOST_DEVICE__ static void add_element_quadpt_residual(
-    const int iquad,
-    const T xpts[xpts_per_elem], 
-    const T vars[dof_per_elem], 
-    const Data physData,
-    T res[dof_per_elem]
-  ) {
+      const int iquad, const T xpts[xpts_per_elem], const T vars[dof_per_elem],
+      const Data physData, T res[dof_per_elem]) {
     T pt[2];
     T weight = Quadrature::getQuadraturePoint(iquad, pt);
 
-    A2D::Mat<T,Geo::spatial_dim,2> J, Jinv;
+    A2D::Mat<T, Geo::spatial_dim, 2> J, Jinv;
     Geo::interpParamGradient(pt, xpts, J.get_data());
     T detJ;
     A2D::MatInv(J, Jinv);
@@ -42,7 +38,8 @@ class ElementGroup {
 
     // Compute state gradient in parametric space
     A2D::Mat<T, Phys::vars_per_node, 2> dUdxi, dUdxi_bar;
-    Basis::template interpParamGradient<Phys::vars_per_node>(pt, vars, dUdxi.get_data());
+    Basis::template interpParamGradient<Phys::vars_per_node>(pt, vars,
+                                                             dUdxi.get_data());
 
     // Transform to gradient in real space dudx = dudxi * J^-1
     A2D::Mat<T, Phys::vars_per_node, Geo::spatial_dim> dUdx, dUdx_bar;
@@ -53,26 +50,22 @@ class ElementGroup {
     // Compute weak residual (derivative of energy w.r.t state gradient)
     Phys::template computeWeakRes<T>(physData, scale, dUdx, dUdx_bar);
 
-    A2D::MatMatMult<A2D::MatOp::TRANSPOSE, A2D::MatOp::NORMAL>(dUdx_bar, Jinv, dUdxi_bar);
+    A2D::MatMatMult<A2D::MatOp::TRANSPOSE, A2D::MatOp::NORMAL>(dUdx_bar, Jinv,
+                                                               dUdxi_bar);
 
-    Basis::template addInterpParamGradientSens<Phys::vars_per_node>(pt, dUdxi_bar.get_data(), res);
-
+    Basis::template addInterpParamGradientSens<Phys::vars_per_node>(
+        pt, dUdxi_bar.get_data(), res);
   }
 
   template <class Data>
   __HOST_DEVICE__ static void add_element_quadpt_jacobian_col(
-    const int iquad,
-    const int ideriv,
-    const T xpts[xpts_per_elem], 
-    const T vars[dof_per_elem], 
-    const Data physData,
-    T res[dof_per_elem],
-    T matCol[dof_per_elem]
-  ) {
+      const int iquad, const int ideriv, const T xpts[xpts_per_elem],
+      const T vars[dof_per_elem], const Data physData, T res[dof_per_elem],
+      T matCol[dof_per_elem]) {
     T pt[2];
     T weight = Quadrature::getQuadraturePoint(iquad, pt);
 
-    A2D::Mat<T,Geo::spatial_dim,2> J, Jinv;
+    A2D::Mat<T, Geo::spatial_dim, 2> J, Jinv;
     Geo::interpParamGradient(pt, xpts, J.get_data());
     T detJ;
     A2D::MatInv(J, Jinv);
@@ -80,7 +73,8 @@ class ElementGroup {
 
     // compute disp grad from q
     A2D::Mat<T, Phys::vars_per_node, 2> dUdxi;
-    Basis::template interpParamGradient<Phys::vars_per_node>(pt, vars, dUdxi.get_data());
+    Basis::template interpParamGradient<Phys::vars_per_node>(pt, vars,
+                                                             dUdxi.get_data());
     A2D::Mat<T, Phys::vars_per_node, Geo::spatial_dim> dUdx;
     A2D::MatMatMult(dUdxi, Jinv, dUdx);
 
@@ -88,9 +82,10 @@ class ElementGroup {
     const int pertNode = ideriv % Basis::num_nodes;
     const int pertVar = ideriv / Basis::num_nodes;
     A2D::Mat<T, Phys::vars_per_node, 2> dUdxi_dot;
-    // Put the gradient of the pertNode'th basis function in the pertVar'th row of dUdxi_dot
-    Basis::getBasisGrad(pertNode, pt, &dUdxi_dot.get_data()[2*pertVar]);
-    
+    // Put the gradient of the pertNode'th basis function in the pertVar'th row
+    // of dUdxi_dot
+    Basis::getBasisGrad(pertNode, pt, &dUdxi_dot.get_data()[2 * pertVar]);
+
     // Transform dUdxi_dot to physical coordinates
     A2D::Mat<T, Phys::vars_per_node, Geo::spatial_dim> dUdx_dot;
     A2D::MatMatMult(dUdxi_dot, Jinv, dUdx_dot);
@@ -98,7 +93,7 @@ class ElementGroup {
     A2D::Mat<FADType, Phys::vars_per_node, Geo::spatial_dim> dUdx_fwd;
     T scale = detJ * weight;
     // Set forward seed into dUdx
-    for (int ii=0; ii<dUdx.ncomp; ii++){
+    for (int ii = 0; ii < dUdx.ncomp; ii++) {
       dUdx_fwd[ii].value = dUdx[ii];
       dUdx_fwd[ii].deriv[0] = dUdx_dot[ii];
     }
@@ -107,88 +102,103 @@ class ElementGroup {
     Phys::template computeWeakRes<FADType>(physData, scale, dUdx_fwd, dUdx_bar);
     // Now dUdx_bar contains the forward seed of the weak res
     A2D::Mat<T, Phys::vars_per_node, Geo::spatial_dim> weak_res_dot, weak_res;
-    for (int ii=0; ii<dUdx.ncomp; ii++){
+    for (int ii = 0; ii < dUdx.ncomp; ii++) {
       weak_res[ii] = dUdx_bar[ii].value;
       weak_res_dot[ii] = dUdx_bar[ii].deriv[0];
     }
 
     A2D::Mat<T, Phys::vars_per_node, 2> dUdxi_bar, dUdxi_bar_dot;
-    A2D::MatMatMult<A2D::MatOp::TRANSPOSE, A2D::MatOp::NORMAL>(weak_res, Jinv, dUdxi_bar);
-    A2D::MatMatMult<A2D::MatOp::TRANSPOSE, A2D::MatOp::NORMAL>(weak_res_dot, Jinv, dUdxi_bar_dot);
+    A2D::MatMatMult<A2D::MatOp::TRANSPOSE, A2D::MatOp::NORMAL>(weak_res, Jinv,
+                                                               dUdxi_bar);
+    A2D::MatMatMult<A2D::MatOp::TRANSPOSE, A2D::MatOp::NORMAL>(
+        weak_res_dot, Jinv, dUdxi_bar_dot);
 
-    Basis::template addInterpParamGradientSens<Phys::vars_per_node>(pt, dUdxi_bar.get_data(), res);
-    Basis::template addInterpParamGradientSens<Phys::vars_per_node>(pt, dUdxi_bar_dot.get_data(), matCol);
-
+    Basis::template addInterpParamGradientSens<Phys::vars_per_node>(
+        pt, dUdxi_bar.get_data(), res);
+    Basis::template addInterpParamGradientSens<Phys::vars_per_node>(
+        pt, dUdxi_bar_dot.get_data(), matCol);
   }
 
   // __device__ void element_jacobian(const T* xpts, const T* vars, T* jac) {}
 
   // template <int32_t elems_per_block = 1>
   template <class Data>
-  static void add_residual(int32_t num_elements, int32_t *geo_conn, int32_t *vars_conn, T *xpts, T *vars, Data *physData, T *residual) {
+  static void add_residual(int32_t num_elements, int32_t *geo_conn,
+                           int32_t *vars_conn, T *xpts, T *vars, Data *physData,
+                           T *residual) {
     using ElemGroup = ElementGroup<T, Geo, Basis, Phys>;
 
-    #ifdef USE_GPU
-      const int elems_per_block = 32;
-      dim3 block(elems_per_block,3);
+#ifdef USE_GPU
+    const int elems_per_block = 32;
+    dim3 block(elems_per_block, 3);
 
-      int nblocks = (num_elements + elems_per_block - 1)/ elems_per_block; 
-      dim3 grid(nblocks);
+    int nblocks = (num_elements + elems_per_block - 1) / elems_per_block;
+    dim3 grid(nblocks);
 
-      // add_residual_gpu<T, ElemGroup, elems_per_block> <<<grid, block>>>(num_elements, geo_conn, vars_conn, X, soln, residual);
-      add_residual_gpu<T, ElemGroup, Data, 1> <<<1, 1>>>(num_elements, geo_conn, vars_conn, xpts, vars, physData, residual);
+    // add_residual_gpu<T, ElemGroup, elems_per_block> <<<grid,
+    // block>>>(num_elements, geo_conn, vars_conn, X, soln, residual);
+    add_residual_gpu<T, ElemGroup, Data, 1><<<1, 1>>>(
+        num_elements, geo_conn, vars_conn, xpts, vars, physData, residual);
 
-      gpuErrchk(cudaDeviceSynchronize());
+    gpuErrchk(cudaDeviceSynchronize());
 
-    #else // CPU data
-      // maybe a way to call add_residual_kernel as same method on CPU
-      // with elems_per_block = 1
-      add_residual_cpu<Data>(num_elements, geo_conn, vars_conn, xpts, vars, physData, residual);
-    #endif
+#else  // CPU data
+    // maybe a way to call add_residual_kernel as same method on CPU
+    // with elems_per_block = 1
+    add_residual_cpu<Data>(num_elements, geo_conn, vars_conn, xpts, vars,
+                           physData, residual);
+#endif
   }
 
   template <class Data>
-  static void add_jacobian(int32_t num_elements, int32_t *geo_conn, int32_t *vars_conn, T *xpts, T *vars, Data *physData, T *residual, T* mat) {
+  static void add_jacobian(int32_t num_vars_nodes, int32_t num_elements,
+                           int32_t *geo_conn, int32_t *vars_conn, T *xpts,
+                           T *vars, Data *physData, T *residual, T *mat) {
     using ElemGroup = ElementGroup<T, Geo, Basis, Phys>;
 
-    #ifdef USE_GPU
-      const int elems_per_block = 8;
-      dim3 block(elems_per_block, 12, 3);
+#ifdef USE_GPU
+    const int elems_per_block = 8;
+    dim3 block(elems_per_block, 12, 3);
 
-      int nblocks = (num_elements + elems_per_block - 1)/ elems_per_block; 
-      dim3 grid(nblocks);
+    int nblocks = (num_elements + elems_per_block - 1) / elems_per_block;
+    dim3 grid(nblocks);
 
-      // add_residual_gpu<T, ElemGroup, elems_per_block> <<<grid, block>>>(num_elements, geo_conn, vars_conn, X, soln, residual);
-      add_jacobian_gpu<T, ElemGroup, Data, 1> <<<1, 1>>>(num_elements, geo_conn, vars_conn, xpts, vars, physData, residual, mat);
+    // add_residual_gpu<T, ElemGroup, elems_per_block> <<<grid,
+    // block>>>(num_elements, geo_conn, vars_conn, X, soln, residual);
+    add_jacobian_gpu<T, ElemGroup, Data, 1>
+        <<<1, 1>>>(num_vars_nodes, num_elements, geo_conn, vars_conn, xpts,
+                   vars, physData, residual, mat);
 
-      gpuErrchk(cudaDeviceSynchronize());
+    gpuErrchk(cudaDeviceSynchronize());
 
-    #else // CPU data
-      // maybe a way to call add_residual_kernel as same method on CPU
-      // with elems_per_block = 1
-      add_jacobian_cpu<Data>(num_elements, geo_conn, vars_conn, xpts, vars, physData, residual, mat);
-    #endif
+#else  // CPU data
+    // maybe a way to call add_residual_kernel as same method on CPU
+    // with elems_per_block = 1
+    add_jacobian_cpu<Data>(num_vars_nodes, num_elements, geo_conn, vars_conn,
+                           xpts, vars, physData, residual, mat);
+#endif
   }
 
   template <class Data>
-  static void add_residual_cpu(int32_t num_elements, int32_t *geo_conn, int32_t *vars_conn, T *xpts, T *vars, Data *physData, T *residual) {
-        
+  static void add_residual_cpu(int32_t num_elements, int32_t *geo_conn,
+                               int32_t *vars_conn, T *xpts, T *vars,
+                               Data *physData, T *residual) {
     const int nxpts_per_elem = Geo::num_nodes * Geo::spatial_dim;
     const int vars_per_elem = Basis::num_nodes * Phys::vars_per_node;
 
     for (int ielem = 0; ielem < num_elements; ielem++) {
-
       T elem_xpts[nxpts_per_elem];
       T elem_vars[vars_per_elem];
       T elem_res[vars_per_elem];
       Data elem_physData = physData[ielem];
 
       // get values for this element
-      const int32_t *geo_nodes = &geo_conn[ielem*Geo::num_nodes];
+      const int32_t *geo_nodes = &geo_conn[ielem * Geo::num_nodes];
       for (int inode = 0; inode < Geo::num_nodes; inode++) {
         int32_t global_inode = geo_nodes[inode];
-        for (int idim = 0 ; idim < Geo::spatial_dim; idim++) {
-          elem_xpts[inode*Geo::spatial_dim + idim] = xpts[global_inode*Geo::spatial_dim + idim];
+        for (int idim = 0; idim < Geo::spatial_dim; idim++) {
+          elem_xpts[inode * Geo::spatial_dim + idim] =
+              xpts[global_inode * Geo::spatial_dim + idim];
         }
       }
 
@@ -197,12 +207,13 @@ class ElementGroup {
       // }
       // return;
 
-      const int32_t *vars_nodes = &vars_conn[ielem*Basis::num_nodes];
+      const int32_t *vars_nodes = &vars_conn[ielem * Basis::num_nodes];
       for (int inode = 0; inode < Basis::num_nodes; inode++) {
         int global_inode = vars_nodes[inode];
-        for (int idof = 0 ; idof < Phys::vars_per_node; idof++) {
-          elem_vars[inode*Phys::vars_per_node + idof] = vars[global_inode*Phys::vars_per_node + idof];
-          elem_res[inode*Phys::vars_per_node + idof] = 0.0;
+        for (int idof = 0; idof < Phys::vars_per_node; idof++) {
+          elem_vars[inode * Phys::vars_per_node + idof] =
+              vars[global_inode * Phys::vars_per_node + idof];
+          elem_res[inode * Phys::vars_per_node + idof] = 0.0;
         }
       }
 
@@ -210,31 +221,31 @@ class ElementGroup {
 
       // compute element residual
       for (int iquad = 0; iquad < Quadrature::num_quad_pts; iquad++) {
-        add_element_quadpt_residual<Data>(
-          iquad, elem_xpts, elem_vars, elem_physData, elem_res
-        );
+        add_element_quadpt_residual<Data>(iquad, elem_xpts, elem_vars,
+                                          elem_physData, elem_res);
       }
 
       // add back into global res on CPU
-      for (int inode = 0; inode < Basis::num_nodes; inode++) {
-        int global_inode = vars_nodes[inode];
-        for (int idof = 0 ; idof < Phys::vars_per_node; idof++) {
-          residual[global_inode*Phys::vars_per_node + idof] += elem_res[inode*Phys::vars_per_node + idof];
-        }
-      }
-      
-    } // num_elements for loop
+      for (int idof = 0; idof < vars_per_elem; idof++) {
+        int local_inode = idof % Basis::num_nodes;
+        int local_idim = idof / Basis::num_nodes;
+        int iglobal =
+            vars_nodes[local_inode] * Phys::vars_per_node + local_idim;
+        residual[iglobal] += elem_res[idof];
+      }  // end of residual assembly
 
+    }  // num_elements for loop
   }
 
   template <class Data>
-  static void add_jacobian_cpu(int32_t num_elements, int32_t *geo_conn, int32_t *vars_conn, T *xpts, T *vars, Data *physData, T *residual, T* mat) {
-        
+  static void add_jacobian_cpu(int32_t vars_num_nodes, int32_t num_elements,
+                               int32_t *geo_conn, int32_t *vars_conn, T *xpts,
+                               T *vars, Data *physData, T *residual, T *mat) {
     const int nxpts_per_elem = Geo::num_nodes * Geo::spatial_dim;
     const int vars_per_elem = Basis::num_nodes * Phys::vars_per_node;
+    const int num_vars = vars_num_nodes * Phys::vars_per_node;
 
     for (int ielem = 0; ielem < num_elements; ielem++) {
-
       T elem_xpts[nxpts_per_elem];
       T elem_vars[vars_per_elem];
       T elem_res[vars_per_elem];
@@ -245,11 +256,12 @@ class ElementGroup {
       memset(elem_mat, 0.0, vars_per_elem * vars_per_elem * sizeof(T));
 
       // get values for this element
-      const int32_t *geo_nodes = &geo_conn[ielem*Geo::num_nodes];
+      const int32_t *geo_nodes = &geo_conn[ielem * Geo::num_nodes];
       for (int inode = 0; inode < Geo::num_nodes; inode++) {
         int32_t global_inode = geo_nodes[inode];
-        for (int idim = 0 ; idim < Geo::spatial_dim; idim++) {
-          elem_xpts[inode*Geo::spatial_dim + idim] = xpts[global_inode*Geo::spatial_dim + idim];
+        for (int idim = 0; idim < Geo::spatial_dim; idim++) {
+          elem_xpts[inode * Geo::spatial_dim + idim] =
+              xpts[global_inode * Geo::spatial_dim + idim];
         }
       }
 
@@ -258,11 +270,12 @@ class ElementGroup {
       // }
       // return;
 
-      const int32_t *vars_nodes = &vars_conn[ielem*Basis::num_nodes];
+      const int32_t *vars_nodes = &vars_conn[ielem * Basis::num_nodes];
       for (int inode = 0; inode < Basis::num_nodes; inode++) {
         int global_inode = vars_nodes[inode];
-        for (int idof = 0 ; idof < Phys::vars_per_node; idof++) {
-          elem_vars[inode*Phys::vars_per_node + idof] = vars[global_inode*Phys::vars_per_node + idof];
+        for (int idof = 0; idof < Phys::vars_per_node; idof++) {
+          elem_vars[inode * Phys::vars_per_node + idof] =
+              vars[global_inode * Phys::vars_per_node + idof];
         }
       }
 
@@ -270,32 +283,44 @@ class ElementGroup {
 
       // compute element residual
       for (int ideriv = 0; ideriv < vars_per_elem; ideriv++) {
-        A2D::Vec<T, vars_per_elem> matCol; // initialized to zero
+        A2D::Vec<T, vars_per_elem> matCol;  // initialized to zero
         for (int iquad = 0; iquad < Quadrature::num_quad_pts; iquad++) {
-          add_element_quadpt_jacobian_col<Data>(
-            iquad, ideriv, elem_xpts, elem_vars, elem_physData, elem_res, matCol.get_data()
-          );
-        }
+          add_element_quadpt_jacobian_col<Data>(iquad, ideriv, elem_xpts,
+                                                elem_vars, elem_physData,
+                                                elem_res, matCol.get_data());
 
-        for (int idof = 0; idof < vars_per_elem; idof++) {
-          printf("matCol[%d] = %.8e\n", idof, matCol[idof]);
-        }
+          // add into elem_mat first
+          for (int jdof = 0; jdof < vars_per_elem; jdof++) {
+            elem_mat[vars_per_elem * jdof + ideriv] += matCol[jdof];
+          }
 
-        // add into elem_mat first
+          // for (int idof = 0; idof < vars_per_elem; idof++) {
+          //   printf("matCol[%d] = %.8e\n", idof, matCol[idof]);
+          // }
+        }
       }
       // we've computed element stiffness matrix
+      // now we add into global stiffness matrix
 
-      // add back into global res on CPU
-      // for (int inode = 0; inode < Basis::num_nodes; inode++) {
-      //   int global_inode = vars_nodes[inode];
-      //   for (int idof = 0 ; idof < Phys::vars_per_node; idof++) {
-      //     residual[global_inode*Phys::vars_per_node + idof] += elem_res[inode*Phys::vars_per_node + idof];
-      //   }
-      // }
-      
-    } // num_elements for loop
+      // assembly into global matrix
+      for (int idof = 0; idof < vars_per_elem; idof++) {
+        int local_inode = idof % Basis::num_nodes;
+        int local_idim = idof / Basis::num_nodes;
+        int iglobal =
+            vars_nodes[local_inode] * Phys::vars_per_node + local_idim;
+        residual[iglobal] += elem_res[idof];
 
-  }
-  
-}; // end of ElementGroup class
+        for (int jdof = 0; jdof < vars_per_elem; jdof++) {
+          int local_jnode = jdof % Basis::num_nodes;
+          int local_jdim = jdof / Basis::num_nodes;
+          int jglobal =
+              vars_nodes[local_jnode] * Phys::vars_per_node + local_jdim;
+          mat[num_vars * iglobal + jglobal] +=
+              elem_mat[vars_per_elem * idof + jdof];
+        }
+      }  // end of matrix assembly double for loops
 
+    }  // end of num_elements for loop
+  }  // end of addJacobian_cpu method
+
+};  // end of ElementGroup class
--- tests/single_tri_element/main.cu
+++ tests/single_tri_element/main.cu
@@ -1,91 +1,110 @@
 #include "analysis.h"
 
 int main(void) {
-    using T = double;
+  using T = double;
 
-    const A2D::GreenStrainType strain = A2D::GreenStrainType::LINEAR;
+  const A2D::GreenStrainType strain = A2D::GreenStrainType::LINEAR;
 
-    using Quad = TriangleQuadrature<T>;
-    using Geo = LinearTriangleGeo<T,Quad>;
-    using Basis = QuadraticTriangleBasis<T,Quad>;
-    using Physics = PlaneStressPhysics<T,Quad,strain>;
-    using Group = ElementGroup<T, Geo, Basis, Physics>;
-    using Data = typename Physics::IsotropicData;
-    using Assembler = ElementAssembler<T, Group>;
+  using Quad = TriangleQuadrature<T>;
+  using Geo = LinearTriangleGeo<T, Quad>;
+  using Basis = QuadraticTriangleBasis<T, Quad>;
+  using Physics = PlaneStressPhysics<T, Quad, strain>;
+  using Group = ElementGroup<T, Geo, Basis, Physics>;
+  using Data = typename Physics::IsotropicData;
+  using Assembler = ElementAssembler<T, Group>;
 
-    int num_geo_nodes = 3;
-    int num_vars_nodes = 6;
-    int num_elements = 1;
+  int num_geo_nodes = 3;
+  int num_vars_nodes = 6;
+  int num_elements = 1;
 
-    // simple test for a singular triangular element
-    // of a mesh with:
-    //      node 0 - (0,0)
-    //      node 1 - (2,1)
-    //      node 2 - (1,2)
+  // simple test for a singular triangular element
+  // of a mesh with:
+  //      node 0 - (0,0)
+  //      node 1 - (2,1)
+  //      node 2 - (1,2)
 
-    // make fake element connectivity for testing
-    int N = Geo::num_nodes * num_elements;
-    int32_t geo_conn[N] = {0,1,2};
+  // make fake element connectivity for testing
+  int N = Geo::num_nodes * num_elements;
+  int32_t geo_conn[N] = {0, 1, 2};
 
-    // randomly generate the connectivity for the variables / basis
-    int N2 = Basis::num_nodes * num_elements;
-    int32_t vars_conn[N2] = {0,1,2,3,4,5};
+  // randomly generate the connectivity for the variables / basis
+  int N2 = Basis::num_nodes * num_elements;
+  int32_t vars_conn[N2] = {0, 1, 2, 3, 4, 5};
 
-    // set the xpts randomly for this example
-    int32_t num_xpts = Geo::spatial_dim * num_geo_nodes;
-    T xpts[num_xpts] = {0, 0, 2, 1, 1, 2};
+  // set the xpts randomly for this example
+  int32_t num_xpts = Geo::spatial_dim * num_geo_nodes;
+  T xpts[num_xpts] = {0, 0, 2, 1, 1, 2};
 
-    // initialize ElemData
-    double E = 70e9, nu = 0.3, t = 0.005; // aluminum plate
-    Data elemData[num_elements];
-    for (int ielem = 0; ielem < num_elements; ielem++) {
-        elemData[ielem] = Data(E, nu, t);
+  // initialize ElemData
+  double E = 70e9, nu = 0.3, t = 0.005;  // aluminum plate
+  Data elemData[num_elements];
+  for (int ielem = 0; ielem < num_elements; ielem++) {
+    elemData[ielem] = Data(E, nu, t);
+  }
+
+  // make the assembler
+  Assembler assembler(num_geo_nodes, num_vars_nodes, num_elements, geo_conn,
+                      vars_conn, xpts, elemData);
+
+  // disp field is u = (x+y)*alpha, v = (x-y)*alpha
+  int32_t num_vars = assembler.get_num_vars();
+  T h_vars[num_vars] = {0.0, 0.0, 3, 1, 3, -1, 3, 0, 1.5, -0.5, 1.5, 0.5};
+
+  // scale by alpha later
+  T alpha = 1.0;
+  for (int idof = 0; idof < num_vars; idof++) {
+    h_vars[idof] *= alpha;
+  }
+
+#ifdef USE_GPU
+  T *d_vars;
+  cudaMalloc((void **)&d_vars, num_vars * sizeof(T));
+  cudaMemcpy(d_vars, h_vars, num_vars * sizeof(T), cudaMemcpyHostToDevice);
+  assembler.set_variables(d_vars);
+#else  // USE_GPU
+  assembler.set_variables(h_vars);
+#endif
+
+  // define the residual vector (host or device)
+  T *h_residual = new T[num_vars];
+  memset(h_residual, 0.0, num_vars * sizeof(T));
+#ifdef USE_GPU
+  T *d_residual;
+  cudaMalloc((void **)&d_residual, num_vars * sizeof(T));
+  cudaMemset(d_residual, 0.0, num_vars * sizeof(T));
+#endif
+
+  int num_vars2 = num_vars * num_vars;
+  T *h_mat = new T[num_vars2];
+  memset(h_mat, 0.0, num_vars2 * sizeof(T));
+#ifdef USE_GPU
+  T *d_mat;
+  cudaMalloc((void **)&d_mat, num_vars2 * sizeof(T));
+  cudaMemset(d_mat, 0.0, num_vars2 * sizeof(T));
+#endif
+
+  // #ifdef USE_GPU
+  // assembler.add_residual(d_residual);
+  // cudaMemcpy(h_residual, d_residual, num_vars * sizeof(T),
+  // cudaMemcpyDeviceToHost); #else assembler.add_residual(h_residual); #endif
+
+  // call add jacobian
+#ifdef USE_GPU
+  assembler.add_jacobian(d_residual, d_mat);
+  cudaMemcpy(h_residual, d_residual, num_vars * sizeof(T),
+             cudaMemcpyDeviceToHost);
+  cudaMemcpy(h_mat, d_mat, num_vars * sizeof(T), cudaMemcpyDeviceToHost);
+#else
+  assembler.add_jacobian(h_residual, h_mat);
+#endif
+
+  // print data of host residual
+  for (int i = 0; i < num_vars; i++) {
+    printf("res[%d] = %.8e\n", i, h_residual[i]);
+    for (int j = 0; j < num_vars; j++) {
+      printf("mat[%d,%d] = %.8e\n", i, j, h_mat[num_vars * i + j]);
     }
+  }
 
-    // make the assembler
-    Assembler assembler(num_geo_nodes, num_vars_nodes, num_elements, 
-      geo_conn, vars_conn, xpts, elemData);
-
-    // disp field is u = (x+y)*alpha, v = (x-y)*alpha
-    int32_t num_vars = assembler.get_num_vars();
-    T h_vars[num_vars] = {0.0, 0.0, 3, 1, 3, -1, 3, 0, 1.5, -0.5, 1.5, 0.5};
-
-    // scale by alpha later
-    T alpha = 1.0;
-    for (int idof = 0; idof < num_vars; idof++) {
-      h_vars[idof] *= alpha;
-    }
-
-    #ifdef USE_GPU
-    T *d_vars;
-    cudaMalloc((void**)&d_vars, num_vars * sizeof(T));
-    cudaMemcpy(d_vars, h_vars, num_vars * sizeof(T), cudaMemcpyHostToDevice);   
-    assembler.set_variables(d_vars);
-    #else // USE_GPU
-    assembler.set_variables(h_vars);
-    #endif
-    
-
-    // define the residual vector (host or device)
-    T *h_residual = new T[num_vars];
-    memset(h_residual, 0.0, num_vars * sizeof(T));
-    #ifdef USE_GPU
-    T *d_residual;
-    cudaMalloc((void**)&d_residual, num_vars * sizeof(T));
-    cudaMemset(d_residual, 0.0, num_vars * sizeof(T));     
-    #endif
-
-    #ifdef USE_GPU
-    assembler.add_residual(d_residual);
-    cudaMemcpy(h_residual, d_residual, num_vars * sizeof(T), cudaMemcpyDeviceToHost);
-    #else
-    assembler.add_residual(h_residual);
-    #endif
-
-    // print data of host residual
-    for (int i = 0; i < num_vars; i++) {
-      printf("res[%d] = %.8e\n", i, h_residual[i]);
-    }
-
-    return 0;
-}
+  return 0;
+};
