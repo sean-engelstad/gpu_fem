a23e611b87bd55573d912a5eac1f0bd34e95127d
Sean Engelstad <sean.engelstad1@gmail.com>
2025-01-28 12:00:54 -0500

    fix permute vec kernel

--- include/linalg/vec.cuh
+++ include/linalg/vec.cuh
@@ -9,14 +9,19 @@ __GLOBAL__ void apply_vec_bcs_kernel(Vec<int> bcs, T *data) {
 }
 
 template <typename T, template <typename> class Vec>
-__GLOBAL__ void permute_vec_kernel(int N, T *data, T *temp, int block_dim, int *perm) {
+__GLOBAL__ void permute_vec_kernel(int num_nodes, T *data, T *temp, int block_dim, int *perm) {
     
     // TODO : could also have each block in the grid do more work with for loop
     // for larger problems?
 
     // copying data from data to temp using permutation
-    int orig_idof = blockIdx.x * blockDim.x + threadIdx.x;
-    int perm_idof = Vec<T>::permuteDof(orig_idof, perm, block_dim);
-    T val = data[orig_idof];
-    temp[perm_idof] = val;
+    int orig_inode = blockIdx.x * blockDim.x + threadIdx.x;
+    if (orig_inode < num_nodes) {
+        for (int inner_dof = 0; inner_dof < block_dim; inner_dof++) {
+        int orig_idof = block_dim * orig_inode + inner_dof;
+        int perm_idof = Vec<T>::permuteDof(orig_idof, perm, block_dim);
+        T val = data[orig_idof];
+        temp[perm_idof] = val;
+    }
+    }
 }
