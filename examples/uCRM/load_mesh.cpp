716e4eadb2d512203122876c22addf7e36c25341
seanfireball1 <sean.engelstad1@gmail.com>
2025-01-30 09:33:24 -0500

    prelim mesh loader work

---
+++ examples/uCRM/load_mesh.cpp
@@ -0,0 +1,9 @@
+#include "../../include/mesh/TACSMeshLoader.h"
+
+int main() {
+    TACSMeshLoader<double> loader{};
+    loader.scanBDFFile("CRM_box_2nd.bdf");
+
+    int num_nodes = loader.getNumNodes();
+    printf("num_nodes = %d\n", num_nodes);
+};
